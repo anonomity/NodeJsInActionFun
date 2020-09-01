d6f53a058f54fcf438f5cd2d5649e6839763bf40
Jackelyn Velez <jlauravelez@gmail.com>
2020-09-01 18:37:14 +0200

    Added ability to shutdown the server with the command shutdown

--- chatserver.js
+++ chatserver.js
@@ -11,15 +11,36 @@ channel.on('join', function(id, client) {
 		}
 	};
 	this.on('broadcast', this.subscriptions[id]);
+
+});
+channel.on('shutdown', () => {
+	channel.emit('broadcast', '', 'the server has shut down. \n');
+	channel.removeAllListeners('broadcast');
+});
+channel.on('leave', function(id){
+	channel.removeListener(
+		'broadcast', this.subscriptions[id]
+	);
+	channel.emit('broadcast', id, `${id} has left the chatroom. \n`);
 });
 const server = net.createServer(client => {
 	const id = `${client.remoteAdress}:${client.remotePort}`;
 	channel.emit('join', id, client);
 	client.on('data', data => {
 		data = data.toString();
+		if (data === 'shutdown\r\n') {
+		      channel.emit('shutdown');
+		}
+
 		channel.emit('broadcast', id, data);
 	});
+	client.on('close', () => {
+		channel.emit('leave', id);
+	});
+
+
 });
-channel.on('leave', function()
+
+
 server.listen(8888);
 
