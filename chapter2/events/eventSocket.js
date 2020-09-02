1c6f25be893eddb5a379c878c6e2ce786c252c68
Jackelyn Velez <jlauravelez@gmail.com>
2020-09-02 12:45:25 +0200

    fixing bugs

--- chapter2/events/eventSocket.js
+++
@@ -1,9 +0,0 @@
-const net = require('net');
-const server = net.createServer(socket => {
-	socket.once('data', data => {
-		socket.write("she wrote\n");
-		socket.write(data);
-		console.log(...data)
-		});
-});
-server.listen(8888);
---
+++ chapter2/events/newEventEmitter.js
@@ -0,0 +1,16 @@
+//modified from original by firing event once after user types
+const net = require('net');
+
+const EventEmitter = require('events').EventEmitter;
+const channel = new EventEmitter();
+channel.on('join', () => {
+	  console.log('Welcome!');
+});
+
+const server = net.createServer(socket => {
+	socket.once('data', data => {
+		channel.emit('join');
+	});
+});
+
+server.listen(8888);
