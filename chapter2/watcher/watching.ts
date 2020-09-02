e6b1c51238884f7f8efa6b0a1d6ec19d93747fec
Jackelyn Velez <jlauravelez@gmail.com>
2020-09-02 08:45:51 +0200

    watching class monitors fs which emits resp and send file to new location

---
+++ chapter2/watcher/watching.ts
@@ -0,0 +1,17 @@
+const Watcher = require('./watcher.ts');
+const fs = require('fs');
+
+const watchDir = './test'
+const processedDir = './testlocation'
+
+const watcher = new Watcher(watchDir, processedDir);
+
+watcher.on('process', (file) => {
+	const watchFile = `${watchDir}/${file}`;
+	const processedFile = `${processedDir}/${file.toLowerCase()}`;
+	fs.rename(watchFile, processedFile, err => {
+		if (err) throw err;
+	});
+});
+
+watcher.start();
