79ee48a5906ca4e3b62f65a8cf3a4b8ddfab63a0
Jackelyn Velez <jlauravelez@gmail.com>
2020-09-15 18:04:11 +0200

    sqlite scaffold

--- chapter3/index.ts
+++ chapter3/index.ts
@@ -1,21 +1,33 @@
 import express from 'express'
 const app = express();
-const articles = [{title: 'Example'}];
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+
+const Article = require('./db').Article;
 
 app.set('port', process.env.PORT || 3000);
 
 app.get('/articles', (req: express.Request, res: express.Response) => {
-    res.send(articles)
+	Article.all(err, articles) => {
+		if (err) return next(err);
+		res.send(articles)
 })
 
 app.post('/articles', (req: express.Request, res: express.Response) => {
-    res.send('Ok')
+	const article = {title:  req.body.title };
+	articles.push(article)
+	res.send(article)
 })
 
 app.get('/articles/:id', (req: express.Request, res: express.Response) => {
-    const id : any = req.params.id;
-    console.log('Fetching:' , id)
-    res.send(articles[id]);
+	const id = req.params.id
+	Article.find(id, (err, article) => {
+		if(err) return next(err);
+		res.send(article)
+	})
 })
 
 app.delete('/articles/:id', (req: express.Request, res: express.Response) => {
